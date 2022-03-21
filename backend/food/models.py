dfec35b0578306aede690fae859858edf88bf6d1
Никита Чалых <Wizardus@list.ru>
2022-03-21 12:57:05 +0300

    v0.2

--- backend/food/models.py
+++ backend/food/models.py
@@ -4,13 +4,45 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class Formula(models.Model):
+class Tag(models.Model):
+    title = models.CharField(
+        verbose_name='Название тега',
+        max_length=50,
+        unique=True
+    )
+    color = models.CharField(
+        verbose_name='Цветовой HEX-код тега',
+        max_length=100,
+        unique=True
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Слаг тега'
+    )
+
+
+class Ingredient(models.Model):
     author = models.ForeignKey(
         User,
+        verbose_name='Автор рецепта',
         on_delete=models.CASCADE,
         related_name='formulas'
     )
     title = models.CharField(
+        verbose_name='Название рецепта',
+        max_length=200
+    )
+
+
+class Formula(models.Model):
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор рецепта',
+        on_delete=models.CASCADE,
+        related_name='formulas'
+    )
+    title = models.CharField(
+        verbose_name='Название рецепта',
         max_length=200
     )
     image = models.ImageField(
@@ -22,29 +54,21 @@ class Formula(models.Model):
         verbose_name='Текст рецепта',
         help_text='Текстовое описание рецепта'
     )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Ингридиенты рецепта'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Тэги рецепта'
+    )
+    time = models.DurationField(
+        verbose_name='Время приготолвения рецепта',
+    )
 
     class Meta:
-        ordering = ['-pub_date']
+        verbose_name = "Рецепт"
+        verbose_name_plural = "Рецепты"
 
     def __str__(self):
         return self.title[:15]
-
-
-# class Comment(models.Model):
-#     text = models.TextField(
-#         verbose_name='Текст комментария',
-#         help_text='Текст вашего комментария'
-#     )
-#     author = models.ForeignKey(
-#         User,
-#         on_delete=models.CASCADE,
-#         related_name='comments'
-#     )
-#     post = models.ForeignKey(
-#         Post,
-#         on_delete=models.CASCADE,
-#         related_name='comments'
-#     )
-
-#     def __str__(self):
-#         return self.text[:15]
