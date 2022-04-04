616e87dabb2d83ed8de96570062d7e4f8755eab1
Никита Чалых <Wizardus@list.ru>
2022-04-04 09:21:42 +0300

    v1.11

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,5 @@
+from io import StringIO
+
 from django.contrib.auth.hashers import check_password
 from django.db.models import Sum
 from django.http import HttpResponse
@@ -9,13 +11,13 @@ from rest_framework.response import Response
 from food.models import (AmountIngredient, FavoriteRecipe, Ingredient, Recipe,
                          ShoppingList, Tag)
 from users.models import Follow, User
-
 from .permissions import RecipePermission, UserPermission
 from .serializers import (FollowSerializer, FullRecipeSerializer,
                           IngredientSerializer, PasswordSerializer,
                           RecordRecipeSerializer, SmallRecipeSerializer,
                           TagSerializer, UserSerializer)
-from .utils import PageLimitPaginator, delete_old_ingredients
+from .utils import (IngredientFilterBackend, PageLimitPaginator,
+                    RecipeFilterBackend, delete_old_ingredients)
 
 
 class UserViewSet(
@@ -119,15 +121,7 @@ class TagViewSet(
 class IngredientViewSet(TagViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-
-    def get_queryset(self):
-        name = self.request.query_params.get('name')
-        if name is not None:
-            regular_name = '^' + name
-            return Ingredient.objects.filter(
-                name__regex=regular_name
-            )
-        return Ingredient.objects.all()
+    filter_backends = (IngredientFilterBackend,)
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -135,38 +129,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
     serializer_class = FullRecipeSerializer
     pagination_class = PageLimitPaginator
     permission_classes = (RecipePermission,)
+    filter_backends = (RecipeFilterBackend,)
 
     def get_serializer_class(self):
         if self.action == 'create' or self.action == 'partial_update':
             return RecordRecipeSerializer
         return FullRecipeSerializer
-
-    def get_queryset(self):
-        is_favorited = self.request.query_params.get('is_favorited')
-        is_in_shopping_cart = self.request.query_params.get(
-            'is_in_shopping_cart'
-        )
-        recipes_author = self.request.query_params.get('author')
-        recipes_tags = self.request.query_params.getlist('tags')
-        review_queryset = Recipe.objects.all()
-        if is_favorited == '1':
-            review_queryset = review_queryset.filter(
-                favorite_recipes__user=self.request.user
-            )
-        if is_in_shopping_cart == '1':
-            review_queryset = review_queryset.filter(
-                shopping_list_recipes__user=self.request.user
-            )
-        if recipes_author is not None:
-            review_queryset = review_queryset.filter(
-                author=recipes_author
-            )
-        if recipes_tags != []:
-            regular_tags = '|'.join(recipes_tags)
-            review_queryset = review_queryset.filter(
-                tags__slug__regex=regular_tags
-            )
-        return review_queryset.distinct()
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -186,19 +154,21 @@ class RecipeViewSet(viewsets.ModelViewSet):
         shopping_list = shopping_list.values('ingredient').annotate(
             average_amount=Sum('amount')
         )
-        with open("shopping_list.txt", "w") as file:
-            for position in shopping_list:
-                position_ingredient = Ingredient.objects.get(
-                    pk=position['ingredient']
-                )
-                position_amount = position['average_amount']
-                file.write(
-                    f' *  {position_ingredient.name.title()}'
-                    f' ({position_ingredient.measurement_unit})'
-                    f' - {position_amount}' + '\n'
-                )
-        file = open('shopping_list.txt')
-        response = HttpResponse(file, content_type='text')
+        myfile = StringIO()
+        for position in shopping_list:
+            position_ingredient = Ingredient.objects.get(
+                pk=position['ingredient']
+            )
+            position_amount = position['average_amount']
+            myfile.write(
+                f' *  {position_ingredient.name.title()}'
+                f' ({position_ingredient.measurement_unit})'
+                f' - {position_amount}' + '\n'
+            )
+        response = HttpResponse(
+            myfile.getvalue(),
+            content_type='text'
+        )
         response['Content-Disposition'] = (
             'attachment; filename="%s"' % 'shopping_list.txt'
         )
@@ -208,60 +178,57 @@ class RecipeViewSet(viewsets.ModelViewSet):
 class ShoppingCartViewSet(viewsets.ViewSet):
     permission_classes = (permissions.IsAuthenticated,)
 
+    def return_serialized_data(self, recipe, request):
+        serializer = SmallRecipeSerializer(
+            recipe, context={'request': request}
+        )
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
     def create(self, request, id):
-        recipe_in_shopping_cart = get_object_or_404(Recipe, pk=id)
-        if not recipe_in_shopping_cart.shopping_list_recipes.filter(
+        recipe = get_object_or_404(Recipe, pk=id)
+        if not recipe.shopping_list_recipes.filter(
             user=request.user
         ).exists():
             ShoppingList.objects.create(
                 user=request.user,
-                recipe=recipe_in_shopping_cart
+                recipe=recipe
             )
-            serializer = SmallRecipeSerializer(
-                recipe_in_shopping_cart, context={'request': request}
-            )
-            return Response(serializer.data, status.HTTP_201_CREATED)
+            return self.return_serialized_data(recipe, request)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    def destroy(self, request, id):
-        recipe_in_shopping_cart = get_object_or_404(Recipe, pk=id)
-        data_shopping_list = (
-            recipe_in_shopping_cart.shopping_list_recipes.filter(
-                user=request.user
-            )
-        )
-        if data_shopping_list.exists():
-            data_shopping_list.delete()
+    def check_and_delete_data(self, data):
+        if data.exists():
+            data.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
+    def destroy(self, request, id):
+        recipe = get_object_or_404(Recipe, pk=id)
+        data = (
+            recipe.shopping_list_recipes.filter(
+                user=request.user
+            )
+        )
+        return self.check_and_delete_data(data)
 
-class FavoriteViewSet(viewsets.ViewSet):
-    permission_classes = (permissions.IsAuthenticated,)
+
+class FavoriteViewSet(ShoppingCartViewSet):
 
     def create(self, request, id):
-        favorite_recipe = get_object_or_404(Recipe, pk=id)
-        if not favorite_recipe.favorite_recipes.filter(
+        recipe = get_object_or_404(Recipe, pk=id)
+        if not recipe.favorite_recipes.filter(
             user=request.user
         ).exists():
             FavoriteRecipe.objects.create(
                 user=request.user,
-                recipe=favorite_recipe
+                recipe=recipe
             )
-            serializer = SmallRecipeSerializer(
-                favorite_recipe, context={'request': request}
-            )
-            return Response(serializer.data, status.HTTP_201_CREATED)
+            return self.return_serialized_data(recipe, request)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, id):
-        favorite_recipe = get_object_or_404(Recipe, pk=id)
-        data_favorite = (
-            favorite_recipe.favorite_recipes.filter(
-                user=request.user
-            )
+        recipe = get_object_or_404(Recipe, pk=id)
+        data = (
+            recipe.favorite_recipes.filter(user=request.user)
         )
-        if data_favorite.exists():
-            data_favorite.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return self.check_and_delete_data(data)
