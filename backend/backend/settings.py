a97940b8c357bc266e4e47b64caddaccd0f86149
Никита Чалых <Wizardus@list.ru>
2022-04-06 19:38:16 +0300

    docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
