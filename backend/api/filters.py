ce891a5fae064f433e272adf1b8f202a8f4fc471
Никита Чалых <Wizardus@list.ru>
2022-04-08 12:14:26 +0300

    v1.6

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -6,7 +6,7 @@ class IngredientFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         name = request.query_params.get('name')
         if name is not None:
-            regular_name = '^' + name
+            regular_name = '^' + name + '|' + name
             return queryset.filter(
                 name__regex=regular_name
             )
