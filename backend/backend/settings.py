4e258a472746e952ad19aa0d19301f96402bbcc2
Никита Чалых <Wizardus@list.ru>
2022-04-10 18:07:41 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default=')h@+luvguw*z4ug83b3@jlmyk=6+0t3)s(
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'food.apps.FoodConfig',
